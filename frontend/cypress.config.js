cdae526129b2dfe90054198872c5bc287c8c00e4
Tom <tomleesm@gmail.com>
2023-01-16 20:38:50 +0800

    重新產生 Vue 專案並安裝 Cypress
    設定好 package.json，讓 npm run test:e2e 可以正確執行 Cypress
    Cypress 固定版本 12.0.0

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:4173'
   },
-};
+  component: {
+    specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite'
+    }
+  }
+})
